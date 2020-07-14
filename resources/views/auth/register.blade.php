d949d6a2f604a334e671c7586055b6c886e2a140
amsyarDev <61406633+amsyarDev@users.noreply.github.com>
2020-07-14 18:01:13 +0800

    register page done

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -19,7 +19,7 @@
                 <div class="col-md-12">
                     <div class="form-group">
                       <label for="kategori">Kategori</label>
-                      <select id="kategori" class="custom-select  bg-light @error('kategori') is-invalid @enderror" name="kategori" value="{{ old('kategori') }}" autofocus>
+                      <select id="kategori" class="custom-select  bg-light @error('kategori') is-invalid @enderror" name="kategori" value="{{ old('kategori') }}" onchange="showJenisForm(this)" autofocus>
                             <option value="kementerian">Kementerian</option>
                             <option value="agensi">Agensi</option>
                             <option value="penyelidik">Penyelidik</option>
@@ -50,7 +50,7 @@
                 <div class="col-md-6">
                     <div class="form-group">
                         <label for="kad_pengenalan" class="text-muted">Kad Pengenalan (*nombor sahaja)</label>
-                        <input id="kad_pengenalan" type="text" class="form-control bg-light @error('kad_pengenalan') is-invalid @enderror" name="kad_pengenalan" value="{{ old('kad_pengenalan') }}" autocomplete="kad_pengenalan" autofocus>
+                        <input id="kad_pengenalan" type="text" onkeyup="get_tarikh_lahir()" class="form-control bg-light @error('kad_pengenalan') is-invalid @enderror" name="kad_pengenalan" value="{{ old('kad_pengenalan') }}" autocomplete="kad_pengenalan" autofocus>
                         @error('kad_pengenalan')
                         <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
@@ -79,7 +79,7 @@
                 <div class="col-md-6">
                     <div class="form-group">
                         <label for="tarikh_lahir" class="text-muted">Tarikh Lahir</label>
-                        <input id="tarikh_lahir" type="date" class="form-control bg-light @error('tarikh_lahir') is-invalid @enderror" name="tarikh_lahir" value="{{ old('tarikh_lahir') }}" autocomplete="tarikh_lahir" autofocus>
+                        <input id="tarikh_lahir" type="text" class="form-control bg-light @error('tarikh_lahir') is-invalid @enderror" name="tarikh_lahir" value="{{ old('tarikh_lahir') }}" autocomplete="tarikh_lahir" readonly>
                         @error('tarikh_lahir')
                         <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
@@ -142,7 +142,7 @@
             </div>
             <div class="row">
                 <!-- Nama Kementerian/Jabatan/Badan Berkanun/Swasta -->
-                <div class="col-md-12">
+                <div class="col-md-12" id="nama_kementerian_div" style="display: block;">
                     <div class="form-group">
                       <label for="nama_kementerian" class="text-muted">Nama Kementerian/Jabatan/Badan Berkanun/Swasta</label>
                       <input id="nama_kementerian" type="text" class="form-control bg-light @error('nama_kementerian') is-invalid @enderror" name="nama_kementerian" value="{{ old('nama_kementerian') }}" autocomplete="nama_kementerian" autofocus>
@@ -155,8 +155,22 @@
                 </div>
             </div>
             <div class="row">
+                <!-- Bahagian -->
+                <div class="col-md-12" id="bahagian_div" style="display: none;">
+                    <div class="form-group">
+                      <label for="bahagian" class="text-muted">Bahagian</label>
+                      <input id="bahagian" type="text" class="form-control bg-light @error('bahagian') is-invalid @enderror" name="bahagian" value="{{ old('bahagian') }}" autocomplete="bahagian" autofocus>
+                      <!-- @error('bahagian')
+                      <span class="invalid-feedback" role="alert">
+                              <strong>{{ $message }}</strong>
+                          </span>
+                      @enderror -->
+                    </div>
+                </div>
+            </div>
+            <div class="row">
                 <!-- Alamat Kementerian/Jabatan/Badan Berkanun/Swasta -->
-                <div class="col-md-12">
+                <div class="col-md-12" id="alamat_kementerian_div" style="display: block;">
                     <div class="form-group">
                       <label for="alamat_kementerian" class="text-muted">Alamat Kementerian/Jabatan/Badan Berkanun/Swasta</label>
                       <input id="alamat_kementerian" type="text" class="form-control bg-light @error('alamat_kementerian') is-invalid @enderror" name="alamat_kementerian" value="{{ old('alamat_kementerian') }}" autocomplete="alamat_kementerian" autofocus>
@@ -226,7 +240,52 @@
         </form>
         </div>
         </div>
-
-
     </div>
+    <script type="text/javascript">
+      function showJenisForm(select){
+        if(select.value=='awam'){
+          document.getElementById('nama_kementerian_div').style.display = "none";
+          document.getElementById('alamat_kementerian_div').style.display = "none";
+          document.getElementById('bahagian_div').style.display = "none";
+        }else if(select.value=='Dalaman'){
+          document.getElementById('bahagian_div').style.display = "block";
+        }else {
+          document.getElementById('nama_kementerian_div').style.display = "block";
+          document.getElementById('alamat_kementerian_div').style.display = "block";
+          document.getElementById('bahagian_div').style.display = "block";
+        }
+
+      }
+
+      function get_tarikh_lahir(){
+        var ic = document.getElementById("kad_pengenalan").value;
+
+        var Year = ic.substring(0, 2);
+        var Month = ic.substring(2, 4);
+        var Day = ic.substring(4, 6);
+
+        var cutoff = (new Date()).getFullYear() - 2000;
+
+        // var dob = Day + "/" + Month + "/" + (Year > cutoff ? '19' : '20') + Year;
+
+        var dob = (Year > cutoff ? '19' : '20') + Year + "/" + Month + "/"  + Day;
+
+        // $('#tarikh_lahir').val(dob);
+
+        console.log(dob);
+
+        var tarikh_lahir = convert(dob);
+
+        console.log(tarikh_lahir);
+
+        document.getElementById("tarikh_lahir").value = tarikh_lahir;
+
+      }
+      function convert(str) {
+        var date = new Date(str),
+          mnth = ("0" + (date.getMonth() + 1)).slice(-2),
+          day = ("0" + date.getDate()).slice(-2);
+        return [day, mnth, date.getFullYear()].join("/");
+      }
+    </script>
 @endsection
