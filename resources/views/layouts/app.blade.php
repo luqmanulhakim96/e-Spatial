33dd96935ad5e82fb9101fc0fe0c91fb77bb3fae
luqmanulhakim96 <hakim9608@gmail.com>
2020-07-23 14:16:35 +0800

    change mix to asset for qbadminuit/css/main.css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
     <!-- Stylesheet -->
     <link rel="stylesheet" href="{{ asset('qbadminui/fontawesome/css/all.min.css') }}">
     <link rel="stylesheet" href="{{ asset('qbadminui/css/vendor/bootstrap-4.3.1/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ mix('qbadminui/css/main.css') }}">
+    <link rel="stylesheet" href="{{ asset('qbadminui/css/main.css') }}">
     <link rel="stylesheet" href="{{ asset('qbadminui/css/vendor/DataTable-1.10.20/datatables.min.css') }}"></link>
 
     <!-- TinyMCE script -->
