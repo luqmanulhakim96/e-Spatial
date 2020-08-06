91e6f6d4660892cc5e10175c4718d6d12f74a3fc
amsyarDev <61406633+amsyarDev@users.noreply.github.com>
2020-08-06 14:20:29 +0800

    landing page
    - fix typo in modal DataPermohonan

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,14 +11,91 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+
 
         <!-- Styles -->
         <style>
+.btn {
+  border: 2px solid white;
+  background-color: white;
+  color: black;
+  padding: 20px 28px;
+  font-size: 20px;
+  width: 140px;
+  cursor: pointer;
+}
+
+/* Green */
+.success {
+  background-color: Transparent;
+  color: green;
+}
+
+.success:hover {
+  background-color: #ffff;
+  color: black;
+}
+
+/* Blue */
+.info {
+  border-color: #2196F3;
+  color: dodgerblue;
+}
+
+.info:hover {
+  background: #2196F3;
+  color: white;
+}
+
+/* Orange */
+.warning {
+  border-color: #ff9800;
+  color: orange;
+}
+
+.warning:hover {
+  background: #ff9800;
+  color: white;
+}
+
+/* Red */
+.danger {
+  border-color: #f44336;
+  color: red;
+}
+
+.danger:hover {
+  background: #f44336;
+  color: white;
+}
+
+/* Gray */
+.default {
+  border-color: #e7e7e7;
+  color: black;
+}
+
+.default:hover {
+  background: #e7e7e7;
+}
+</style>
+        <style>
             html, body {
-                background-color: #fff;
-                color: #636b6f;
+                /* background-color: #000; */
+                background-image: url('https://images.pexels.com/photos/1423600/pexels-photo-1423600.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940');
+                background-repeat: no-repeat;
+                background-attachment: fixed;
+                background-size: 100% 100%;
+                background-color: rgba(0,0,0,0.5);
+                    background-blend-mode: darken;
+                /* opacity: 0.7; */
+                color: #fff;
                 font-family: 'Nunito', sans-serif;
-                font-weight: 200;
+                font-weight: 400;s
                 height: 100vh;
                 margin: 0;
             }
@@ -38,9 +115,10 @@
             }
 
             .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
+                position: center;
+                bottom: 8rem;
+                right: 750px;
+
             }
 
             .content {
@@ -52,9 +130,9 @@
             }
 
             .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
+                color: #fff;
+                padding: 15px 25px;
+                font-size: 15px;
                 font-weight: 600;
                 letter-spacing: .1rem;
                 text-decoration: none;
@@ -64,27 +142,17 @@
             .m-b-md {
                 margin-bottom: 30px;
             }
+
+
+
+
         </style>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Log Masuk</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Daftar</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
             <div class="content">
                 <div class="title m-b-md">
-                    JPSM e-Spatial
+                     e-Spatial
                 </div>
 
                 <div class="links">
@@ -94,7 +162,22 @@
                 <div class="links">
                   <a href="https://www.artaniscloud.com/">made by Artanis Cloud Sdn. Bhd.</a>
                 </div>
+                @if (Route::has('login'))
+                    <div class="top-right links" style="margin-top: 20px;">
+                        @auth
+                            <a class="btn success" href="{{ url('/home') }}">Home</a>
+                        @else
+                            <a class="btn success" style="margin-right: 5px;" href="{{ route('login') }}">Log Masuk</a>
+
+                            @if (Route::has('register'))
+                                <a class="btn success" href="{{ route('register') }}">Daftar</a>
+                            @endif
+                        @endauth
+                    </div>
+                @endif
             </div>
+
+
         </div>
     </body>
 </html>
