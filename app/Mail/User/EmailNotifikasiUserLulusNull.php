0fa638ad7593998ca79f15e43dc8263335ab4f35
luqmanulhakim96 <hakim9608@gmail.com>
2020-10-28 11:39:04 +0800

    changes in mail

--- app/Mail/User/EmailNotifikasiUserLulusNull.php
+++ app/Mail/User/EmailNotifikasiUserLulusNull.php
@@ -32,8 +32,8 @@ class EmailNotifikasiUserLulusNull extends Mailable
     public function build()
     {
       return $this->to($this->admin->email , $this->admin->name)
-              ->from(env('MAIL_FROM_ADDRESS'))
-              // ->from('system@espatial.com')
+              // ->from(env('MAIL_FROM_ADDRESS'))
+              ->from('espatial@forestry.gov.my')
               ->subject('Permohonan Lulus')
               ->view('senarai-email.templates.notifikasiUserPermohonanLulusNull');
     }
