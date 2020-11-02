585e463d77c9d370e20c1214442a631b0d201142
luqmanulhakim96 <hakim9608@gmail.com>
2020-11-02 12:58:00 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Support\ServiceProvider;
 use App\User;
 use Auth;
 use App\Permohonan;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,6 +28,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+      Schema::defaultStringLength(191);
+
       view()->composer('*', function ($view)
       {
         if(Auth::user()){
