fc36653f462d1b7600b9c7de3532e379e9c633b1
luqmanulhakim96 <hakim9608@gmail.com>
2020-11-02 12:14:50 +0800

    emailc hanges

--- app/Mail/Admin/EmailNotifikasiAdminResitPembayaranNull.php
+++ app/Mail/Admin/EmailNotifikasiAdminResitPembayaranNull.php
@@ -32,8 +32,8 @@ class EmailNotifikasiAdminResitPembayaranNull extends Mailable
     public function build()
     {
       return $this->to($this->admin->email , $this->admin->name)
-              ->from(env('MAIL_FROM_ADDRESS'))
-              // ->from('espatial@forestry.gov.my')
+              // ->from(env('MAIL_FROM_ADDRESS'))
+              ->from('espatial@forestry.gov.my')
               ->subject('Resit Pembyaran Telah dimuatnaik')
               ->view('senarai-email.templates.notifikasiAdminResitPembayaranNull');
     }
