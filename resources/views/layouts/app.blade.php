642ed132b978ccb89b852ed2525a2e10f614cf07
amsyarDev <amsyaraazhar97@gmail.com>
2021-03-02 14:34:04 +0800

    push
    - display laporan and status perlaksanaan tu super admin

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -149,6 +149,21 @@
 
                         <li class="side-menu-item px-3"><a href="{{ route('superadmin.auditTrailLogUser') }}" class="w-100 py-3 pl-4" >Audit Trail Log Akses</a></li>
 
+                        <li class="side-menu-item px-3"><a href="{{ route('permohonan.statusPerlaksanaan') }}" class="w-100 py-3 pl-4" >Status Perlaksanaan</a></li>
+
+                        <li class="side-menu-item px-3"><a href="#" class="w-100 py-3 pl-4 sub-menu-parent" data-toggle="collapse" data-target="#table-sub-menu-laporan" aria-expanded="false" aria-controls="table-sub-menu">Laporan</a></li>
+                        <div id="table-sub-menu-laporan" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+                            <ul class="side-sub-menu p-0">
+                                <li class="side-sub-menu-item px-3"><a href="{{route('laporan.laporan-1')}}" class="w-100 pl-4">Laporan Bilangan Keseluruhan Permohonan</a></li>
+                                <li class="side-sub-menu-item px-3"><a href="{{route('laporan.laporan-2')}}" class="w-100 pl-4">Laporan Bilangan Permohonan Mengikut Kategori Permohon</a></li>
+                                <li class="side-sub-menu-item px-3"><a href="{{route('laporan.laporan-3')}}" class="w-100 pl-4">Laporan Mengikut Kategori Data Yang Dipohon</a></li>
+                                <li class="side-sub-menu-item px-3"><a href="{{route('laporan.laporan-4')}}" class="w-100 pl-4">Laporan Mengikut Status Permohonan</a></li>
+                                <li class="side-sub-menu-item px-3"><a href="{{route('laporan.laporan-5')}}" class="w-100 pl-4">Laporan Mengikut Kategori Data Yang Diluluskan</a></li>
+                                <li class="side-sub-menu-item px-3"><a href="{{route('laporan.laporan-6')}}" class="w-100 pl-4">Laporan Mengikut Kategori Data Yang Tidak Diluluskan</a></li>
+                                <li class="side-sub-menu-item px-3"><a href="{{route('laporan.laporan-7')}}" class="w-100 pl-4">Laporan Mengikut Kategori Data Yang Tidak Berkaitan</a></li>
+                                <li class="side-sub-menu-item px-3"><a href="{{route('laporan.laporan-8')}}" class="w-100 pl-4">Laporan Mengikut Kategori Data Yang Batal</a></li>
+                            </ul>
+                        </div>
                         @endif
                     </ul>
                 </div>
